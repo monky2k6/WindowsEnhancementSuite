3e64223e405200cc49ee978b81b5534e8bdd92d9
ts <ts@deltra.de>
2016-01-06 17:07:39 +0100

    Switched Extension Class to Helper Class

---
+++ WindowsEnhancmentSuite/Helper/ThreadHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+
+namespace WindowsEnhancementSuite.Helper
+{
+    public static class ThreadHelper
+    {
+        public static void RunAsStaThread(Action synchronAction)
+        {
+            var thread = new Thread(synchronAction.Invoke) { IsBackground = true };
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
+    }
+}
