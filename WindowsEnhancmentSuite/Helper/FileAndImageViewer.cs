c0729755955b258db6522e96bf817e003dfbf1a4
ts <ts@deltra.de>
2016-01-08 08:28:50 +0100

    Removed unused usings

--- WindowsEnhancmentSuite/Helper/FileAndImageViewer.cs
+++ WindowsEnhancmentSuite/Helper/FileAndImageViewer.cs
@@ -4,7 +4,6 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsEnhancementSuite.Extensions;
 using WindowsEnhancementSuite.Properties;
