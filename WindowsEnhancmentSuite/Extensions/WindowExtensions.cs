9521411fd932efbd426c5a459f201b6f52457214
monky2k6 <monyk2k6@googlemail.com>
2017-06-20 15:47:12 +0200

    Started switching to WPF

---
+++ WindowsEnhancmentSuite/Extensions/WindowExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing;
+using System.Windows;
+
+namespace WindowsEnhancementSuite.Extensions
+{
+    public static class WindowExtensions
+    {
+        public static Rectangle Bounds(this Window window)
+        {
+            var rectangle = new Rectangle
+            {
+                X = (int)window.Left,
+                Y = (int)window.Top,
+                Width = (int)window.Width,
+                Height = (int)window.Height
+            };
+
+            return rectangle;
+        }
+    }
+}
--- WindowsEnhancmentSuite/Services/ApplicationBarService.cs
+++ WindowsEnhancmentSuite/Services/ApplicationBarService.cs
@@ -7,6 +7,13 @@ using System.Windows;
 using System.Windows.Forms;
 using WindowsEnhancementSuite.Bases;
 using WindowsEnhancementSuite.Forms;
+using WindowsEnhancementSuite.Properties;
+using WindowsEnhancementSuite.Extensions;
+using System.IO;
+using System.Windows.Markup;
+using System.Windows.Forms.Integration;
+using System.Windows.Interop;
+using System.Windows.Media.Animation;
 
 namespace WindowsEnhancementSuite.Services
 {
@@ -16,8 +23,11 @@ namespace WindowsEnhancementSuite.Services
         
         private readonly IKeyboardMouseEvents hooks;
         private readonly Rect hoverArea;
-        private readonly ApplicationBarForm barForm;
+        private readonly Window applicationBarWindow;
         private readonly List<DraggableBaseForm> barForms;
+        private readonly DoubleAnimation slideIn;
+        private readonly DoubleAnimation slideOut;
+        private readonly DependencyProperty widthProperty;
 
         private Rectangle barFormHoverArea;
         private bool isBarResizing;
@@ -29,29 +39,58 @@ namespace WindowsEnhancementSuite.Services
             hooks.MouseMove += mouseMoveHook;
             hooks.MouseUp += mouseUpHook;
 
+            // Load WPF Window
+            using (var stream = Resources.ApplicationBar.ToStream<MemoryStream>())
+            {
+                applicationBarWindow = XamlReader.Load(stream) as Window;
+            }
+
+            if (applicationBarWindow == null) return;
+            ElementHost.EnableModelessKeyboardInterop(applicationBarWindow);
+            applicationBarWindow.Topmost = true;
+
             // Create MouseOver-Checkarea for
-            // the left side of the primary screen
             var screenBounds = Screen.PrimaryScreen.WorkingArea;
             hoverArea = new Rect(screenBounds.Left - 2, screenBounds.Top, 5, screenBounds.Bottom);
+
+            // Create and prepare the BarForm
+            applicationBarWindow.Show();
+            applicationBarWindow.Hide();
+            applicationBarWindow.Left = screenBounds.Left;
+            applicationBarWindow.Top = screenBounds.Top;
+            applicationBarWindow.Width = 250;
+            applicationBarWindow.Height = screenBounds.Height;
+
+            // Prepare animations
+            slideIn = new DoubleAnimation(0f, 250f, new Duration(TimeSpan.FromSeconds(3)));
+            widthProperty = DependencyProperty.Register("Width", typeof(double), applicationBarWindow.GetType());
+
+            this.barFormHoverArea = applicationBarWindow.Bounds();
+            this.barFormHoverArea.Inflate(1, 1);
+
+            //// Create MouseOver-Checkarea for
+            //// the left side of the primary screen
+            //var screenBounds = Screen.PrimaryScreen.WorkingArea;
+            //hoverArea = new Rect(screenBounds.Left - 2, screenBounds.Top, 5, screenBounds.Bottom);
 
             // Initiate collections
             barForms = new List<DraggableBaseForm>();
 
             // Create and prepare the BarForm
-            barForm = new ApplicationBarForm();
-            barForm.Location = new System.Drawing.Point((int)screenBounds.Left, (int)screenBounds.Top);
-            barForm.Width = 250; // todo: make it adjustable
-            barForm.Height = (screenBounds.Bottom - screenBounds.Top);
-            barForm.MinimumSize = new System.Drawing.Size(250, barForm.Height);
-            barForm.MaximumSize = new System.Drawing.Size(screenBounds.Width / 2, barForm.Height);
+            //applicationBarWindow = new ApplicationBarForm();
+            //applicationBarWindow.Location = new System.Drawing.Point((int)screenBounds.Left, (int)screenBounds.Top);
+            //applicationBarWindow.Width = 250; // todo: make it adjustable
+            //applicationBarWindow.Height = (screenBounds.Bottom - screenBounds.Top);
+            //applicationBarWindow.MinimumSize = new System.Drawing.Size(250, applicationBarWindow.Height);
+            //applicationBarWindow.MaximumSize = new System.Drawing.Size(screenBounds.Width / 2, applicationBarWindow.Height);
 
-            barForm.ResizeBegin += barForm_ResizeBegin;
-            barForm.ResizeEnd += barForm_ResizeEnd;
-            barForm.VisibleChanged += barForm_VisibleChanged;
-            barForm.Hide();
+            //applicationBarWindow.ResizeBegin += barForm_ResizeBegin;
+            //applicationBarWindow.ResizeEnd += barForm_ResizeEnd;
+            //applicationBarWindow.VisibleChanged += barForm_VisibleChanged;
+            //applicationBarWindow.Hide();
 
             // Calculte MouseHoverArea
-            this.barForm_ResizeEnd(barForm, EventArgs.Empty);
+            //this.barForm_ResizeEnd(applicationBarWindow, EventArgs.Empty);
         }
 
         public void Dispose()
@@ -107,19 +146,30 @@ namespace WindowsEnhancementSuite.Services
 
         private void sortForms()
         {
-            int formHeight = 10;
-            foreach (var f in barForms.OrderBy(e => e.Top))
-            {
-                if (f.IsResizing)
-                    f.ResizeValues = new Rectangle(0, formHeight, barForm.Width - ApplicationBarForm.DRAG_SPACE, f.Height);
-                else
-                {
-                    f.Top = formHeight;
-                    f.Left = 0;
-                    f.Width = barForm.Width - ApplicationBarForm.DRAG_SPACE;
-                }
-                formHeight += f.Height + 5;
-            }
+            //int formHeight = 10;
+            //foreach (var f in barForms.OrderBy(e => e.Top))
+            //{
+            //    if (f.IsResizing)
+            //        f.ResizeValues = new Rectangle(0, formHeight, applicationBarWindow.Width - ApplicationBarForm.DRAG_SPACE, f.Height);
+            //    else
+            //    {
+            //        f.Top = formHeight;
+            //        f.Left = 0;
+            //        f.Width = applicationBarWindow.Width - ApplicationBarForm.DRAG_SPACE;
+            //    }
+            //    formHeight += f.Height + 5;
+            //}
+        }
+
+        private void doSlideIn()
+        {
+            applicationBarWindow.Show();
+            applicationBarWindow.BeginAnimation(widthProperty, slideIn);
+        }
+
+        private void doSlideOut()
+        {
+
         }
 
         private void barForm_ResizeBegin(object sender, EventArgs e)
@@ -129,44 +179,45 @@ namespace WindowsEnhancementSuite.Services
 
         private void barForm_ResizeEnd(object sender, EventArgs e)
         {
-            this.barFormHoverArea = barForm.Bounds;
-            this.barFormHoverArea.Inflate(1, 1);
-            this.sortForms();
+            //this.barFormHoverArea = applicationBarWindow.Bounds;
+            //this.barFormHoverArea.Inflate(1, 1);
+            //this.sortForms();
 
-            this.isBarResizing = false;
+            //this.isBarResizing = false;
         }
 
         private void barForm_VisibleChanged(object sender, EventArgs e)
         {
-            foreach (var f in barForms)
-            {
-                if (DragForm == f) continue;
-                f.Visible = barForm.Visible;
-                if (f.Visible)
-                    f.BringToFront();
-            }
+            //foreach (var f in barForms)
+            //{
+            //    if (DragForm == f) continue;
+            //    f.Visible = applicationBarWindow.IsVisible;
+            //    if (f.Visible)
+            //        f.BringToFront();
+            //}
         }
 
         private void mouseMoveHook(object sender, MouseEventArgs e)
         {
-            if (!barForm.IsDisposed && !barForm.Visible)
+            if (applicationBarWindow.IsLoaded && !applicationBarWindow.IsVisible)
                 if (hoverArea.Contains(e.X, e.Y))
-                    barForm.Show();
+                    doSlideIn();
+                    //applicationBarWindow.Show();
 
-            if (barForm.Visible)
-                if (!this.isBarResizing)
+            if (applicationBarWindow.IsVisible)
+                //if (!this.isBarResizing)
                     if (Screen.GetWorkingArea(e.Location).Contains(e.Location))
                         if (!this.barFormHoverArea.Contains(e.X, e.Y))
-                            barForm.Hide();
+                            applicationBarWindow.Hide();
         }
 
         private void mouseUpHook(object sender, MouseEventArgs e)
         {
             if (DragForm != null)
                 if (e.Button == MouseButtons.Left)
-                    if (barForm.Visible)
+                    if (applicationBarWindow.IsVisible)
                     {
-                        if (barForm.Bounds.Contains(e.X, e.Y))
+                        if (applicationBarWindow.Bounds().Contains(e.X, e.Y))
                             if (DragForm.IsAttached)
                                 sortForms();
                             else
